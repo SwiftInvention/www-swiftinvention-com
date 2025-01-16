4621036102681ebab9e0f832d1b0df694a5ff588
saiapowainur <saiapow.ainur@gmail.com>
2025-01-16 17:26:44 +0700

    feat: added second home block

--- new-website/tailwind.config.ts
+++ new-website/tailwind.config.ts
@@ -17,7 +17,13 @@ export default {
       secondaryCol6: '#C2C2C2',
       primaryCol3WithOpacity: 'rgba(255, 123, 83, 0.05)',
       surface: {
-        DEFAULT: '#020617',
+        DEFAULT: '#020617'
+      },
+      title: {
+        DEFAULT: '#0F172A'
+      },
+      body: {
+        DEFAULT: '#1E293B'
       },
       white: '#fff',
       gray: {
@@ -74,6 +80,12 @@ export default {
         xl: ['1.0625rem', '1.5rem'],
         "xl-tablet": ['1.25rem', '1.9375rem'],
         '2.5xl': ['1.75rem', '2rem'],
+        '3xl': ['1.375rem', '2rem'],
+        '3xl-tablet': ['1.875rem', '2.5rem'],
+        'lg': ['1rem', '1.5rem'],
+        'lg-tablet': ['1.125rem', '1.75rem'],
+        '6xl': ['2.25rem', '3rem', 600],
+        '6xl-tablet': ['3.75rem', '4.25rem', 600],
         '7xl': ['3rem', '3.5rem', 600],
         '7xl-tablet': ['4.5rem', '5rem', 600]
       },
