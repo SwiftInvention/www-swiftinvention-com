c25486abc7bbb17e1c6f0330714ada7af6e2952e
Anthony Ivanov <aivanov@swiftinvention.com>
2026-03-20 14:10:32 +0100

    Tailwind transitions fix

--- src/react/components/careerscard/StackCard.tsx
+++ src/react/components/careerscard/StackCard.tsx
@@ -12,7 +12,7 @@ export const StackCard: React.FC<StackCardProps> = ({ text, image, link }) => {
       <a
         href={link}
         target="_blank"
-        className="flex h-full w-full flex-row items-center justify-start gap-6 bg-white/5 p-4 shadow-card hover:shadow-cardHovered md:p-6"
+        className="flex h-full w-full flex-row items-center justify-start gap-6 bg-white/5 p-4 shadow-card hover:shadow-card-hovered md:p-6"
       >
         <img className="h-14 w-14 object-contain" src={image} alt="service" />
         <div className="text-left font-sans text-lg font-normal text-white">
